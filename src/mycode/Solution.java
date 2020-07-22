58dd45642093fa90eb8aad20d9cae04b4b0137b2
Lamire <873619492@qq.com>
2020-07-22 11:07:29 +0800

    2020 7 22

--- src/mycode/Solution.java
+++ src/mycode/Solution.java
@@ -13,7 +13,7 @@ class TestSolution {
         int[] pushed = new int[]{7, 6, 5, 4};
         int[] popped = new int[]{4, 3, 5, 2, 1, 10};
         int[][] events = {{2, 3}, {2, 3}, {1, 5}, {1, 5}, {1, 5}};
-        
+        solution.generateTrees(3);
     }
 }
 public class Solution {
@@ -878,6 +878,37 @@ public class Solution {
                 break;
         }
         return (int) res;
+    }
+    //95. 不同的二叉搜索树II   code again
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+    public List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> allTrees = new LinkedList<>();
+        if (start > end) {
+            allTrees.add(null);
+            return allTrees;
+        }
+        // 枚举可行根节点
+        for (int i = start; i <= end; i++) {
+            // 获得所有可行的左子树集合
+            List<TreeNode> leftTrees = generateTrees(start, i - 1);
+            // 获得所有可行的右子树集合
+            List<TreeNode> rightTrees = generateTrees(i + 1, end);
+            // 从左子树集合中选出一棵左子树，从右子树集合中选出一棵右子树，拼接到根节点上
+            for (TreeNode left : leftTrees) {
+                for (TreeNode right : rightTrees) {
+                    TreeNode currTree = new TreeNode(i);
+                    currTree.left = left;
+                    currTree.right = right;
+                    allTrees.add(currTree);
+                }
+            }
+        }
+        return allTrees;
     }
     //next
 }
